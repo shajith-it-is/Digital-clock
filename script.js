7d7a4d7fab8181c51e390c7c99c43971031c5b21
ShajithKAJ <shajith.it.is@gmail.com>
2022-03-04 21:00:57 +0530

    removed unnecessary codes

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ const secondsNeedle = bodyElement.querySelector('.secondsNeedle');
 const hoursNeedle = bodyElement.querySelector('.hoursNeedle');
 const minutesNeedle = bodyElement.querySelector('.minutesNeedle');
 
-// secondsNeedle.style.transform = `rotate(270deg)`;
-// minutesNeedle.style.transform = `rotate(270deg)`;
-// hoursNeedle.style.transform = `rotate(270deg)`;
-
 function switchChange() {
     if(switchSlider.checked == true) {
         timeDigital.style.display = 'none';
@@ -25,8 +21,6 @@ function switchChange() {
     }
 }
 
-setInterval(runningTime,1000);
-
 function runningTime() {
     const time = new Date();
     hoursElement.innerHTML = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
@@ -35,9 +29,6 @@ function runningTime() {
     secondsNeedle.style.transform = `rotate(${time.getSeconds()*6-90}deg)`;
     minutesNeedle.style.transform = `rotate(${time.getMinutes()*6-90}deg)`;
     hoursNeedle.style.transform = `rotate(${time.getHours() > 12 ? (time.getHours() - 12)*30-90 : time.getHours()*30-90}deg)`;
-
-    //document.getElementById('3').innerHTML = time.getSeconds()*6;
-    // document.getElementById('2').innerHTML = time.getMinutes()*4;
-    // document.getElementById('1').innerHTML = time.getHours();
-    //debugger
 }
+
+setInterval(runningTime,1000);
